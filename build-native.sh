d16a764b20be26f402ee4bdb99b85db6c4ede86d
Andreas Mautsch <amautsch>
2021-05-23 11:52:55 +0200

    tests added

--- build-native.sh
+++ build-native.sh
@@ -2,7 +2,6 @@
 git pull
 time mvn clean spring-boot:build-image -P docker-image-native
 #time mvn clean spring-boot:build-image install -P docker-image-native
-docker run --name person-service-native --rm -p50800:50800 goafabric/person-service-native:1.0.0-SNAPSHOT
 
 #docker pull goafabric/person-service-native:1.0.0-SNAPSHOT && docker run --name person-service-native --rm -p50800:50800 goafabric/person-service-native:1.0.0-SNAPSHOT -Xmx64m
 
