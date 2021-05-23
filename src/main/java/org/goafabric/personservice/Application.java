45ad06e7362265334e91b6985aebb6133e96047f
Andreas Mautsch <amautsch>
2021-05-23 10:19:11 +0200

    basic frame added

---
+++ src/main/java/org/goafabric/personservice/Application.java
@@ -0,0 +1,28 @@
+package org.goafabric.personservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Created by amautsch on 26.06.2015.
+ */
+
+@SpringBootApplication(proxyBeanMethods = false,
+        exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner init(ApplicationContext context) {
+        return args -> {if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {SpringApplication.exit(context, () -> 0);}};
+    }
+}
