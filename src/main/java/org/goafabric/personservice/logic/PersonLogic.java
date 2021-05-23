1e01fb7d5dd954057972713a0e95f82df2d52449
Andreas Mautsch <amautsch>
2021-05-23 10:50:49 +0200

    welcome page added

--- src/main/java/org/goafabric/personservice/logic/PersonLogic.java
+++ src/main/java/org/goafabric/personservice/logic/PersonLogic.java
@@ -3,28 +3,29 @@ package org.goafabric.personservice.logic;
 import org.goafabric.personservice.service.dto.Person;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonLogic {
     public Person getById(String id) {
-        return null;
+        return new Person();
     }
 
     public List<Person> findAll() {
-        return null;
+        return new ArrayList<>();
     }
 
     public List<Person> findByFirstName(String firstName) {
-        return null;
+        return new ArrayList<>();
     }
 
     public List<Person> findByLastName(String lastName) {
-        return null;
+        return new ArrayList<>();
     }
 
     public Person save(Person person) {
-        return null;
+        return new Person();
     }
 
 }
