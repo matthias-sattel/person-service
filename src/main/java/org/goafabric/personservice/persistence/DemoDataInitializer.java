2f795f711cf590fa9e90d3fed4be5d4d7c862b7a
Andreas Mautsch <amautsch>
2021-05-24 09:34:56 +0200

    changes

--- src/main/java/org/goafabric/personservice/persistence/DemoDataInitializer.java
+++ src/main/java/org/goafabric/personservice/persistence/DemoDataInitializer.java
@@ -1,26 +1,24 @@
 package org.goafabric.personservice.persistence;
 
-import org.goafabric.personservice.logic.PersonLogic;
-import org.goafabric.personservice.service.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DemoDataInitializer {
     @Autowired
-    private PersonLogic personLogic;
+    private PersonRepository personRepository;
 
     public void run() {
-        if (personLogic.findAll().size() == 0) {
-            personLogic.save(Person.builder()
+        if (!personRepository.findAll().iterator().hasNext()) {
+            personRepository.save(PersonBo.builder()
                     .firstName("Homer").lastName("Simpson")
                     .build());
 
-            personLogic.save(Person.builder()
+            personRepository.save(PersonBo.builder()
                     .firstName("Bart").lastName("Simpson")
                     .build());
 
-            personLogic.save(Person.builder()
+            personRepository.save(PersonBo.builder()
                     .firstName("Monty").lastName("Burns")
                     .build());
         }
