5d6bb2a633c8a60d4d1784481bfb478a3d77bc25
Andreas Mautsch <amautsch>
2021-06-08 14:22:56 +0200

    adapter fixed

--- src/main/java/org/goafabric/personservice/adapter/CalleeServiceClient.java
+++ src/main/java/org/goafabric/personservice/adapter/CalleeServiceClient.java
@@ -2,7 +2,6 @@ package org.goafabric.personservice.adapter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,20 +9,13 @@ import org.springframework.web.client.RestTemplate;
 //@Lazy //Lazy init, in case we need to reencrypt the password (cylic problem during startup)
 public class CalleeServiceClient {
     @Autowired
-    private RestTemplate calleeServiceRestTemplate;
+    private RestTemplate restTemplate;
 
     @Value("${adapter.calleeservice.url}")
     private String url;
 
     public Boolean isAlive() {
-        return calleeServiceRestTemplate.getForObject(url + "/callees/isAlive", Boolean.class);
+        return restTemplate.getForObject(url + "/callees/isAlive", Boolean.class);
     }
-
-    @Bean
-    public RestTemplate calleeServiceRestTemplate(
-            @Value("${adapter.calleeservice.user}") String user,
-            @Value("${adapter.calleeservice.password}") String password,
-            @Value("${adapter.timeout}") Integer timeout) {
-        return RestTemplateFactory.createRestTemplate(timeout, user, password);
-    }
+    
 }
--- src/main/java/org/goafabric/personservice/adapter/RestTemplateConfiguration.java
+++ src/main/java/org/goafabric/personservice/adapter/RestTemplateConfiguration.java
@@ -1,22 +1,27 @@
 package org.goafabric.personservice.adapter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
 import java.util.Collections;
 
-public class RestTemplateFactory {
-    private RestTemplateFactory() {
-    }
+@Configuration
+public class RestTemplateConfiguration {
 
-    public static RestTemplate createRestTemplate(final int timeout, final String user, final String password) {
+    @Bean
+    public RestTemplate restTemplate(
+            @Value("${adapter.calleeservice.user}") String user,
+            @Value("${adapter.calleeservice.password}") String password,
+            @Value("${adapter.timeout}") Integer timeout) {
         final RestTemplate restTemplate = new RestTemplateBuilder()
-                .setConnectTimeout(Duration.ofMillis(timeout))
-                .setReadTimeout(Duration.ofMillis(timeout))
+                //.setConnectTimeout(Duration.ofMillis(timeout))
+                //.setReadTimeout(Duration.ofMillis(timeout))
                 .build();
 
         restTemplate.setMessageConverters(Collections.singletonList(new MappingJackson2HttpMessageConverter()));
