21186ae60374263044346b6f11a9c9a08328dd5d
Andreas Mautsch <amautsch>
2021-06-08 15:02:34 +0200

    enable http

--- src/main/java/org/goafabric/personservice/adapter/CalleeServiceAdapter.java
+++ src/main/java/org/goafabric/personservice/adapter/CalleeServiceAdapter.java
@@ -2,17 +2,22 @@ package org.goafabric.personservice.adapter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
 public class CalleeServiceAdapter {
     @Autowired
-    private CalleeServiceClient calleeServiceClient;
+    private RestTemplate restTemplate;
+
+    @Value("${adapter.calleeservice.url}")
+    private String url;
 
     public Boolean isAlive() {
         log.info("Calling CalleService ...");
-        final Boolean isAlive = calleeServiceClient.isAlive();
+        final Boolean isAlive = restTemplate.getForObject(url + "/callees/isAlive", Boolean.class);
         log.info("got: " + isAlive);
         return isAlive;
     }
