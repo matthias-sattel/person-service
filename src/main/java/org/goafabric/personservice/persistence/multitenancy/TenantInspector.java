998b3ae00e3a059d9a4e5b4da1609eced8d6cdfc
Andreas Mautsch <amautsch>
2021-06-08 13:07:17 +0200

    openj9 heapdump and adapter added

--- src/main/java/org/goafabric/personservice/persistence/multitenancy/TenantInspector.java
+++ src/main/java/org/goafabric/personservice/persistence/multitenancy/TenantInspector.java
@@ -1,22 +1,18 @@
 package org.goafabric.personservice.persistence.multitenancy;
 
-import lombok.extern.slf4j.Slf4j;
 import org.goafabric.personservice.crossfunctional.TenantIdInterceptor;
 import org.hibernate.resource.jdbc.spi.StatementInspector;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class TenantInspector implements StatementInspector {
-    //inspect sqls from entities annotated with @WHERE(".tenant_id = %TENANT_ID%"), we intentionally omit the ' inside the @WHERE, because if the replacement will not take place, the sql preparation will crash
     @Override
     public String inspect(String sql) {
-        if (sql.contains(TenantAware.TENANT_FILTER)) {
-            sql = sql.replace(TenantAware.TENANT_FILTER, "tenant_id = '" + TenantIdInterceptor.getTenantId() + "'");
-        }
-        return sql;
+        return sql.contains(TenantAware.TENANT_FILTER)
+                ? sql.replace(TenantAware.TENANT_FILTER, "tenant_id = '" + TenantIdInterceptor.getTenantId() + "'")
+                : sql;
     }
 
     @Bean
