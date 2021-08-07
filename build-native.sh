a7495ecc04a2a3c813825dc7f8145ff85fc28010
Andreas Mautsch <amautsch>
2021-08-07 13:05:56 +0200

    changes

--- build-native.sh
+++ build-native.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
 time mvn clean install -P docker-image-native
-docker pull goafabric/person-service-native:1.0.7-SNAPSHOT && docker run --name person-service-native --rm -p50800:50800 goafabric/person-service-native:1.0.7-jasypt-SNAPSHOT -Xmx64m
+docker pull goafabric/person-service-native:1.0.7-SNAPSHOT && docker run --name person-service-native --rm -p50800:50800 goafabric/person-service-native:1.0.7-SNAPSHOT -Xmx64m
 
