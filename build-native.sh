d1a1c83dd71356f4d6be663f6f51277d8b195eb2
Andreas Mautsch <amautsch>
2021-08-28 10:59:54 +0200

    plugin management

--- build-native.sh
+++ build-native.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
-time mvn clean install -P docker-image-native
+time mvn clean deploy -P docker-image-native
 docker pull goafabric/person-service-native:1.1.1-SNAPSHOT && docker run --name person-service-native --rm -p50800:50800 goafabric/person-service-native:1.1.1-SNAPSHOT -Xmx64m
 
