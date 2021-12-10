50c4e6d85ba1abd561d7ac7a463b237298e7444b
Andreas Mautsch <amautsch>
2021-12-10 11:34:24 +0100

    changes

--- build-native.sh
+++ build-native.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
 time mvn clean deploy -P docker-image-native
-docker pull goafabric/person-service-native:1.1.2-SNAPSHOT && docker run --name person-service-native --rm -p50800:50800 goafabric/person-service-native:1.1.2-SNAPSHOT -Xmx64m
+docker pull goafabric/person-service-native:1.1.2-native-11-SNAPSHOT && docker run --name person-service-native --rm -p50800:50800 goafabric/person-service-native:1.1.2-native-11-SNAPSHOT -Xmx64m
 
