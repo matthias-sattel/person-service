78aedd49a9dcd2bf85737a98fb51660e4f179154
Andreas Mautsch <amautsch>
2022-01-22 10:33:14 +0100

    upgrade to spring booot 3

--- src/main/java/org/goafabric/personservice/crossfunctional/TenantIdInterceptor.java
+++ src/main/java/org/goafabric/personservice/crossfunctional/TenantIdInterceptor.java
@@ -1,12 +1,12 @@
 package org.goafabric.personservice.crossfunctional;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 public class TenantIdInterceptor implements WebMvcConfigurer {
