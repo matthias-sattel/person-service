ac33b450d018468521913ec6ffcf44630c57ea2e
Andreas Mautsch <amautsch>
2022-02-18 06:39:04 +0100

    param example added

--- src/main/java/org/goafabric/personservice/persistence/PersonRepository.java
+++ src/main/java/org/goafabric/personservice/persistence/PersonRepository.java
@@ -1,11 +1,15 @@
 package org.goafabric.personservice.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PersonRepository extends JpaRepository<PersonBo, String> {
     List<PersonBo> findByFirstName(String firstName);
-    List<PersonBo> findByLastName(String lastName);
+
+    @Query("SELECT p FROM PersonBo p where p.lastName = :lastName")
+    List<PersonBo> findByLastName(@Param("lastName") String lastName);
 }
 
