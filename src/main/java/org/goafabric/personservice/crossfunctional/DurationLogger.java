1f538628c08c93e425d7f32e785afd07202f2d86
Andreas Mautsch <amautsch>
2022-03-20 10:13:32 +0100

    changes

--- src/main/java/org/goafabric/personservice/crossfunctional/DurationLogger.java
+++ src/main/java/org/goafabric/personservice/crossfunctional/DurationLogger.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.nativex.hint.AotProxyHint;
 import org.springframework.nativex.hint.ProxyBits;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
@@ -31,7 +33,7 @@ public class DurationLogger {
             return joinPoint.proceed();
         } finally {
             final Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
-            log.info("{} took {}ms for user {}", toString(method), System.currentTimeMillis() - startTime, SecurityConfiguration.getUserName());
+            log.info("{} took {}ms for user {}", toString(method), System.currentTimeMillis() - startTime, getUserName());
         }
     }
 
@@ -43,4 +45,8 @@ public class DurationLogger {
                 method.getName(), parameterTypes);
     }
 
+    private String getUserName() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (authentication == null) ? "" : authentication.getName();
+    }
 }
