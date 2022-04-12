cff91e95fae969d2b68b76586ae3d692ab77769a
andreas <andreas>
2022-04-12 13:55:53 +0200

    HttpInterceptor updated

--- src/main/java/org/goafabric/personservice/crossfunctional/DurationLogger.java
+++ src/main/java/org/goafabric/personservice/crossfunctional/DurationLogger.java
@@ -31,7 +31,7 @@ public class DurationLogger {
             return joinPoint.proceed();
         } finally {
             final Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
-            log.info("{} took {}ms for user {}", toString(method), System.currentTimeMillis() - startTime, HttpInterceptor.getUserName());
+            log.info("{} took {}ms for user: {} , tenant: {}", toString(method), System.currentTimeMillis() - startTime, HttpInterceptor.getUserName(), HttpInterceptor.getTenantId());
         }
     }
 
