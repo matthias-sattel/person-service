2d95b17a75fe2dd4d1a5dd8a9653612bbac448a8
Andreas Mautsch <amautsch>
2022-07-24 11:11:39 +0200

    changes

--- src/main/java/org/goafabric/personservice/persistence/multitenancy/TenantInspector.java
+++ src/main/java/org/goafabric/personservice/persistence/multitenancy/TenantInspector.java
@@ -10,14 +10,11 @@ import org.springframework.stereotype.Component;
 public class TenantInspector implements StatementInspector {
     @Override
     public String inspect(String sql) {
-        return sql.contains(TenantAware.TENANT_FILTER)
-                ? sql.replace(TenantAware.TENANT_FILTER, "tenant_id = '" + HttpInterceptor.getTenantId() + "'")
-                : sql;
+        return sql.contains(TenantAware.TENANT_FILTER) ? sql.replace(TenantAware.TENANT_FILTER, "tenant_id = '" + HttpInterceptor.getTenantId() + "'") : sql;
     }
 
     @Bean
     public HibernatePropertiesCustomizer hibernatePropertiesCustomizer() {
-        return hibernateProperties -> hibernateProperties.put("hibernate.session_factory.statement_inspector",
-                TenantInspector.class.getName());
+        return hibernateProperties -> hibernateProperties.put("hibernate.session_factory.statement_inspector", TenantInspector.class.getName());
     }
 }
