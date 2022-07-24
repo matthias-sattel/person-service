7bf085a07f3166187d936952710ba0be6459280f
Andreas Mautsch <amautsch>
2022-07-24 09:05:06 +0200

    authentication changed

--- src/main/java/org/goafabric/personservice/crossfunctional/HttpInterceptor.java
+++ src/main/java/org/goafabric/personservice/crossfunctional/HttpInterceptor.java
@@ -1,8 +1,12 @@
 package org.goafabric.personservice.crossfunctional;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -37,5 +41,14 @@ public class HttpInterceptor implements WebMvcConfigurer {
                 userName.remove();
             }
         });
+    }
+    @Value("${security.authentication.enabled:true}")
+    private Boolean isAuthenticationEnabled;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        if (isAuthenticationEnabled) { http.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().csrf().disable(); }
+        else { http.authorizeRequests().anyRequest().permitAll(); }
+        return http.build();
     }
 }
