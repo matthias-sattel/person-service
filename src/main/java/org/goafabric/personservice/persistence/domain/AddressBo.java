6857e2810c6433a05b5aedcb2a070df07ec97dc0
Andreas Mautsch <amautsch>
2022-08-13 09:12:32 +0200

    merge

--- src/main/java/org/goafabric/personservice/persistence/domain/AddressBo.java
+++ src/main/java/org/goafabric/personservice/persistence/domain/AddressBo.java
@@ -5,7 +5,7 @@ import org.goafabric.personservice.persistence.multitenancy.TenantAware;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Where;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Data
 @Builder
