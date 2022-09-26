7c9057d16378da19ae708bbbe9b95661b9e5e8c6
Andreas Mautsch <amautsch>
2022-09-26 09:30:48 +0200

    zipkin upgrade

--- src/main/java/org/goafabric/personservice/Application.java
+++ src/main/java/org/goafabric/personservice/Application.java
@@ -1,5 +1,7 @@
 package org.goafabric.personservice;
 
+import io.micrometer.observation.ObservationRegistry;
+import jakarta.servlet.DispatcherType;
 import org.goafabric.personservice.adapter.Callee;
 import org.goafabric.personservice.persistence.DatabaseProvisioning;
 import org.springframework.aot.hint.ExecutableMode;
@@ -8,10 +10,13 @@ import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.core.Ordered;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.observation.HttpRequestsObservationFilter;
 
 import java.util.Arrays;
 
@@ -51,7 +56,15 @@ public class Application {
             Arrays.stream(SimpleClientHttpRequestFactory.class.getMethods()).forEach(
                     r -> hints.reflection().registerMethod(r, ExecutableMode.INVOKE));
         }
+    }
 
+    @Bean
+    FilterRegistrationBean traceWebFilter(ObservationRegistry observationRegistry) {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new HttpRequestsObservationFilter(observationRegistry));
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.ASYNC, DispatcherType.ERROR, DispatcherType.FORWARD,
+                DispatcherType.INCLUDE, DispatcherType.REQUEST);
+        filterRegistrationBean.setOrder(Ordered.LOWEST_PRECEDENCE);
+        return filterRegistrationBean;
     }
 
 }
