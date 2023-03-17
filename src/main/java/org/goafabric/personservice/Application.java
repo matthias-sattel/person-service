0c6f48d4660b8c25aac935c7d6b92402c4e7ea54
Andreas Mautsch <amautsch>
2023-03-17 14:37:39 +0100

    changes

--- src/main/java/org/goafabric/personservice/Application.java
+++ src/main/java/org/goafabric/personservice/Application.java
@@ -1,11 +1,15 @@
 package org.goafabric.personservice;
 
+import io.micrometer.observation.ObservationPredicate;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 /**
@@ -25,6 +29,15 @@ public class Application {
         return args -> {
             if ((args.length > 0) && ("-check-integrity".equals(args[0]))) { SpringApplication.exit(context, () -> 0);}
         };
-
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, @Value("${security.authentication.enabled:true}") Boolean isAuthenticationEnabled) throws Exception {
+        return isAuthenticationEnabled ? http.authorizeHttpRequests().anyRequest().authenticated().and().httpBasic().and().csrf().disable().build()
+                : http.authorizeHttpRequests().anyRequest().permitAll().and().build();
+    }
+
+    @Bean
+    ObservationPredicate disableHttpServerObservationsFromName() { return (name, context) -> !name.startsWith("spring.security."); }
+
 }
