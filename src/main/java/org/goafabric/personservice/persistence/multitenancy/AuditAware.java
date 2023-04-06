3bda4eb85ffd4b597b11dd2eb00cd4c8b1225918
Andreas Mautsch <amautsch>
2023-04-06 11:13:49 +0200

    changes

--- src/main/java/org/goafabric/personservice/persistence/multitenancy/AuditAware.java
+++ src/main/java/org/goafabric/personservice/persistence/multitenancy/AuditAware.java
@@ -1,7 +1,9 @@
 package org.goafabric.personservice.persistence.multitenancy;
 
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 
+@MappedSuperclass
 @EntityListeners(AuditListener.class)
 public abstract class AuditAware {
     public abstract String getId();
