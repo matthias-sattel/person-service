180dc56797f8faec5d040e7553837355079eb4c0
Andreas Mautsch <amautsch>
2023-05-30 14:17:41 +0200

    examples added

--- doc/examples/kubernetes-loadbalancer/CalleeServiceAdapterConfiguration.java
+++ doc/examples/kubernetes-loadbalancer/CalleeServiceAdapterConfiguration.java
@@ -12,26 +12,26 @@ import java.time.Duration;
 
 @Configuration
 public class CalleeServiceAdapterConfiguration {
+
+    @Bean
+    public CalleeServiceAdapter calleeServiceAdapter(
+            @Value("${adapter.calleeservice.url}") String url, @Value("${adapter.timeout}") Long timeout, @Value("${adapter.maxlifetime:-1}") Long maxLifeTime) {
+        return createAdapter(CalleeServiceAdapter.class, builder(), url, timeout, maxLifeTime);
+    }
+
     @Bean
     @LoadBalanced
     public WebClient.Builder builder() {
         return WebClient.builder();
     }
 
-    @Bean
-    public CalleeServiceAdapter calleeServiceAdapter(WebClient.Builder builder,
-                                                     @Value("${adapter.calleeservice.url}") String url, @Value("${adapter.timeout}") Long timeout, @Value("${adapter.maxlifetime:-1}") Long maxLifeTime) {
-
-        var client = builder()
-                .clientConnector(getConnector(maxLifeTime))
+    public static <A> A createAdapter(Class<A> adapterType, WebClient.Builder builder, String url, Long timeout, Long maxLifeTime) {
+        var client = builder
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create(
+                        ConnectionProvider.builder("custom").maxLifeTime(Duration.ofMillis(maxLifeTime)).build())))
                 .baseUrl(url).defaultHeaders(header -> header.setBasicAuth("admin", "admin")).build();
         return HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).blockTimeout(Duration.ofMillis(timeout))
-                .build().createClient(CalleeServiceAdapter.class);
-    }
-
-    private ReactorClientHttpConnector getConnector(Long maxLifeTime) {
-        return new ReactorClientHttpConnector(HttpClient.create(
-                ConnectionProvider.builder("custom").maxLifeTime(Duration.ofMillis(maxLifeTime)).build()));
+                .build().createClient(adapterType);
     }
 }
 
