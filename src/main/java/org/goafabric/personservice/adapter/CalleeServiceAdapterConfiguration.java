2511644491fcbffaeeed0fe3a7232cfd85879549
Andreas Mautsch <amautsch>
2023-05-31 15:56:43 +0200

    consul added

--- src/main/java/org/goafabric/personservice/adapter/CalleeServiceAdapterConfiguration.java
+++ src/main/java/org/goafabric/personservice/adapter/CalleeServiceAdapterConfiguration.java
@@ -18,16 +18,11 @@ public class CalleeServiceAdapterConfiguration {
     @Bean
     public CalleeServiceAdapter calleeServiceAdapter(//ReactorLoadBalancerExchangeFilterFunction lbFunction,
             @Value("${adapter.calleeservice.url}") String url, @Value("${adapter.timeout}") Long timeout, @Value("${adapter.maxlifetime:-1}") Long maxLifeTime) {
-        return createAdapter(CalleeServiceAdapter.class, builder(), url, timeout, maxLifeTime);
+        return createAdapter(CalleeServiceAdapter.class, url, timeout, maxLifeTime);
     }
 
-    @Bean
-    //@LoadBalanced
-    public WebClient.Builder builder() {
-        return WebClient.builder();
-    }
-
-    public static <A> A createAdapter(Class<A> adapterType, WebClient.Builder builder, String url, Long timeout, Long maxLifeTime) {
+    public static <A> A createAdapter(Class<A> adapterType, String url, Long timeout, Long maxLifeTime) {
+        var builder = WebClient.builder();
         builder.baseUrl(url)
                 .defaultHeaders(header -> header.setBasicAuth("admin", "admin"))
                 .clientConnector(new ReactorClientHttpConnector(HttpClient.create(ConnectionProvider.builder("custom").maxLifeTime(Duration.ofMillis(maxLifeTime)).build())));
