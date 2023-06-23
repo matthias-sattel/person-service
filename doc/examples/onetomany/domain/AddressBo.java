bb5d0081a17a459ebed9f6e078acf7f1ffe1959a
Andreas Mautsch <amautsch>
2023-06-23 07:13:12 +0200

    changes

---
+++ doc/examples/onetomany/domain/AddressBo.java
@@ -0,0 +1,32 @@
+package org.goafabric.personservice.persistence.domain;
+
+import jakarta.persistence.*;
+import org.goafabric.personservice.persistence.extensions.AuditListener;
+import org.hibernate.annotations.TenantId;
+
+
+@Entity
+@Table(name="address")
+public class AddressBo extends AuditListener.AuditAware {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    public String id;
+
+    @TenantId
+    public String orgunitId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "person_id")
+    public PersonBo myPerson;
+
+    public String street;
+    public String city;
+
+    @Version //optimistic locking
+    public Long version;
+
+    @Override
+    public String getId() {
+        return id;
+    }
+}
