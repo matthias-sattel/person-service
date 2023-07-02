2c9e97280def13c64fbfab2e679926770d3ef389
Andreas Mautsch <amautsch>
2023-07-02 09:33:28 +0200

    audit changes

--- src/main/java/org/goafabric/personservice/repository/extensions/TenantResolver.java
+++ src/main/java/org/goafabric/personservice/repository/extensions/TenantResolver.java
@@ -10,8 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -113,20 +115,19 @@ public class TenantResolver implements CurrentTenantIdentifierResolver, MultiTen
     public ApplicationRunner schemaCreator(Flyway flyway,
                                            @Value("${database.provisioning.goals}") String goals,
                                            @Value("${multi-tenancy.tenants}") String tenants,
-                                           @Value("${multi-tenancy.schema-prefix:_}") String schemaPrefix) {
+                                           @Value("${multi-tenancy.schema-prefix:_}") String schemaPrefix,
+                                           ApplicationContext context) {
         return args -> {
             if (goals.contains("-migrate")) {
                 Arrays.asList(tenants.split(",")).forEach(tenant -> {
-                            Flyway.configure()
-                                    .configuration(flyway.getConfiguration())
-                                    .schemas(schemaPrefix + tenant)
-                                    .defaultSchema(schemaPrefix + tenant)
+                            Flyway.configure().configuration(flyway.getConfiguration())
+                                    .schemas(schemaPrefix + tenant).defaultSchema(schemaPrefix + tenant)
                                     .placeholders(Map.of("tenantId", tenant))
-                                    .load()
-                                    .migrate();
+                                    .load().migrate();
                         }
                 );
             }
+            if (goals.contains("-terminate") && goals.contains("-import")) { SpringApplication.exit(context, () -> 0); }
         };
     }
 
