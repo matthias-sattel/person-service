93acdd8abc6a04740b165bd66cce5579384a3dd1
Andreas Mautsch <amautsch>
2023-07-23 09:59:20 +0200

    coverage badges added

---
+++ src/test/java/org/goafabric/personservice/extensions/ExceptionHandlerTest.java
@@ -0,0 +1,28 @@
+package org.goafabric.personservice.extensions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ExceptionHandlerTest {
+    private ExceptionHandler exceptionHandler = new ExceptionHandler();
+
+    @Test
+    void handleIllegalArgumentException() {
+        assertThat(exceptionHandler.handleIllegalArgumentException(new IllegalArgumentException("illegal argument")).getStatusCode())
+                .isEqualTo(HttpStatus.PRECONDITION_FAILED);
+    }
+
+    @Test
+    void handleIllegalStateException() {
+        assertThat(exceptionHandler.handleIllegalStateException(new IllegalStateException("illegal state")).getStatusCode())
+                .isEqualTo(HttpStatus.PRECONDITION_FAILED);
+    }
+
+    @Test
+    void handleGeneralException() {
+        assertThat(exceptionHandler.handleGeneralException(new IllegalStateException("general failure")).getStatusCode())
+                .isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+}
