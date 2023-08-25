013a91b083a4dadd32d85e1044c2a491205c0f04
Andreas Mautsch <amautsch>
2023-08-25 08:28:11 +0200

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,10 +93,9 @@ jib {
 tasks.register("dockerImageNative") { group = "build"; dependsOn("bootBuildImage") }
 tasks.named<BootBuildImage>("bootBuildImage") {
 	val nativeImageName = "${dockerRegistry}/${project.name}-native" + (if (System.getProperty("os.arch").equals("aarch64")) "-arm64v8" else "") + ":${project.version}"
-	//builder.set(nativeBuilder)
-	if (System.getProperty("os.arch").equals("aarch64")) builder.set(nativeBuilder) else buildpacks.set(listOf("gcr.io/paketo-buildpacks/java-native-image:8.16.0"))
+	builder.set(nativeBuilder)
 	imageName.set(nativeImageName)
-	environment.set(mapOf("BP_NATIVE_IMAGE" to "true", "BP_JVM_VERSION" to "17", "BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to "-J-Xmx4500m -march=compatibility"))
+	environment.set(mapOf("BP_NATIVE_IMAGE" to "true", "BP_JVM_VERSION" to "17", "BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to "-J-Xmx4500m"))
 	doLast {
 		exec { commandLine("docker", "run", "--rm", nativeImageName, "-check-integrity") }
 		exec { commandLine("docker", "push", nativeImageName) }
