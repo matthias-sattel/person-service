8fce6408e66b2ce3257dc20f2d0dba213337f72f
Andreas Mautsch <amautsch>
2023-08-28 07:44:01 +0200

    changes

--- doc/examples/javers/JaversNativeConfiguration.java
+++ doc/examples/javers/JaversNativeConfiguration.java
@@ -175,46 +175,6 @@ public class JaversNativeConfiguration {
 
             });
 
-            /*
-            hints.reflection().registerType(JaversAuditable.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(JaversAuditableAsync.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(JaversAuditableConditionalDelete.class,builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(JaversAuditableDelete.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(JaversSpringDataAuditable.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
-
-            hints.reflection().registerType(JaversSpringDataAuditableRepositoryAspect.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(JaversAuditableAspect.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(JaversSpringDataJpaAuditableRepositoryAspect.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
-
-            hints.reflection().registerType(JaversSqlRepository.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(CommitMetadataRepository.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(GlobalIdRepository.class,builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(CdoSnapshotRepository.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-
-            hints.reflection().registerType(TableNameProvider.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(CdoSnapshotFinder.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(CommitPropertyFinder.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(JaversSchemaManager.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(FixedSchemaFactory.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-
-            hints.reflection().registerType(org.javers.hibernate.integration.HibernateUnproxyObjectAccessHook.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(org.javers.core.json.JsonConverterBuilder.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(org.javers.core.metamodel.type.TypeMapper.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            hints.reflection().registerType(org.javers.core.metamodel.scanner.ClassScanner.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-
-            try {
-                hints.reflection().registerType(Class.forName("org.javers.core.metamodel.scanner.FieldBasedPropertyScanner"), builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            } catch (ClassNotFoundException e) {throw new RuntimeException(e);}
-
-            try {
-                hints.reflection().registerType(Class.forName("org.javers.core.metamodel.scanner.AnnotationNamesProvider"), builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            } catch (ClassNotFoundException e) {throw new RuntimeException(e);}
-            try {
-                hints.reflection().registerType(Class.forName("org.javers.core.metamodel.scanner.ClassAnnotationsScanner"), builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
-            } catch (ClassNotFoundException e) {throw new RuntimeException(e);}
-
-             */
-
         }
     }
 
