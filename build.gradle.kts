a2611a694e790ff4dfaa8a0208c6f35cb3bc6f6c
Andreas Mautsch <amautsch>
2023-09-28 08:53:14 +0200

    upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 group = "org.goafabric"
-version = "3.1.4-SNAPSHOT"
+version = "3.1.5-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 val dockerRegistry = "goafabric"
