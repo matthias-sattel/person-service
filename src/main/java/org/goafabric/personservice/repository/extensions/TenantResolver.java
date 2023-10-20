45f6f349956fafc924d599896b77acf9d1800783
Andreas Mautsch <amautsch>
2023-10-20 16:17:14 +0200

    rc1 upgrade

--- src/main/java/org/goafabric/personservice/repository/extensions/TenantResolver.java
+++ src/main/java/org/goafabric/personservice/repository/extensions/TenantResolver.java
@@ -16,7 +16,6 @@ import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -27,7 +26,6 @@ import java.util.Map;
 // Source: https://spring.io/blog/2022/07/31/how-to-integrate-hibernates-multitenant-feature-with-spring-data-jpa-in-a-spring-boot-application
 
 //@Component
-@DependsOn()
 @ConditionalOnProperty(value = "spring.profiles.active", havingValue = "jpa", matchIfMissing = true)
 @RegisterReflectionForBinding({org.hibernate.binder.internal.TenantIdBinder.class, org.hibernate.generator.internal.TenantIdGeneration.class})
 public class TenantResolver implements CurrentTenantIdentifierResolver, MultiTenantConnectionProvider, HibernatePropertiesCustomizer {
